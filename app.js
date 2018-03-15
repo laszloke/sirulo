2d15106cc001926384e538b137f5b53a5cd92d40
LASZLOKE <LASZLOKE@GMAIL.COM>
2018-03-15 14:59:42 +0000

    servet main structure

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 // fo applikacio
 
+
+
+// initializing the main stuff
+
+// expres variable pointing to the framework
+var express = require("express");
+// main app variable by calling the express function
+var app= express();
+
+
+
+
+
+// start the server in IP and PORT provided by Cloud9 API 
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log(" sirulo szerver is listening on :" + process.env.IP + ":" + process.env.PORT );
+});
+
+
+
+//-----
+//----------
+
+
+
+
+
