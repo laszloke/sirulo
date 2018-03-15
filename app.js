9693273cbb6f107f3ccd699c88528b9a22401b00
LASZLOKE <LASZLOKE@GMAIL.COM>
2018-03-15 15:17:24 +0000

    progress

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ var app= express();
 
 
 
+
+// routes - here we have the routes 
+// landing page
+app.get("/",function(req,res){
+   // res.send("Itt lesz a sirulo website fo oldala");
+    res.render("landing.ejs");
+});
+
+
+
+
+
 // start the server in IP and PORT provided by Cloud9 API 
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log(" sirulo szerver is listening on :" + process.env.IP + ":" + process.env.PORT );
